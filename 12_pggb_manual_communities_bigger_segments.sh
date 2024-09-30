770b6bae12686fb4a5c4762fa2956b8c7af2d09c
Milan Malinsky <mm21@sanger.ac.uk>
2024-09-30 16:03:06 +0100

    add multiple scripts

---
+++ 12_pggb_manual_communities_bigger_segments.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Run PGGB in manually made community files
+# Also increase segment/block size to see if repeats dont get matched in the graph
+
+i=$1 # get the number of the chromosome from the command line
+
+
+
+# Define input and output directories
+INPUTDATA=/lustre/scratch123/tol/projects/cichlid/ldekker/data/communities/chromosome${i}.fa 
+mkdir /lustre/scratch123/tol/projects/cichlid/ldekker/results/12_pggb_manual_communities_bigger_segments/chromosome${i}
+OUTPUTDIR=/lustre/scratch123/tol/projects/cichlid/ldekker/results/12_pggb_manual_communities_bigger_segments/chromosome${i} 
+
+# Index inputfile -> Error indicated necessary
+#~/programs/samtools-1.14/samtools faidx ${INPUTDATA} 
+
+# Run PGGB with the updated full version
+# pggb_full -i ${INPUTDATA} -o ${OUTPUTDIR} -n 4 -t 8 -p 90 -s 10000 -Y '#'
+../software/pggb/pggb -i ${INPUTDATA} -o ${OUTPUTDIR} -n 4 -t 8 -p 90 -s 20k -Y '#' 
+	# -i: path to input file
+	# -o: path to output directory
+	# -n: amount of haplotypes
+	# -t: amount of threads
+	# -p: minimum nucleotide identity
+	# -s: minimum scaffolding length
+	# -Y: delimiter in .fa file
+
+
+
+
